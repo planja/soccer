94400478b98eeaf52a2f1ddbdc38b47b294c5393
planja <6835047dt>
2017-05-14 15:24:03 +0300

    result

--- webui/src/main/webapp/WEB-INF/pages/teams/result.jsp
+++ webui/src/main/webapp/WEB-INF/pages/teams/result.jsp
@@ -22,9 +22,44 @@
 </t:news>
 
 <div class="center-content" ng-controller="resultController">
-    <h1>Результаты матчей</h1>
+    <h1 id="selectedCompetition" style="display: none">Результаты матчей {{selectedCompetition.fullName}}
+        {{selectedCompetition.region}}</h1>
 
     <div class="content">
+        <div>
+            <img ng-src="{{selectedCompetition.flagUrl}}">
+        </div>
+        <select ng-change="changeTeams(selectedCompetition)" ng-model="selectedCompetition"
+                ng-options="item.fullName + item.region for item in competitions">
+        </select>
+
+        <div class="table-data" ng-repeat="m in map">
+            <table>
+
+                <tr>
+                    <td class="date" colspan="4" class="th">{{m.date}}</td>
+                </tr>
+                <tr ng-repeat="match in m.matches">
+                    <td class="time">
+                        {{match.time}}
+                    </td>
+                    <td class="left">
+                        <a href="${pageContext.request.contextPath}/teams/teaminfo/{{match.homeTeam.dbid}}">{{match.homeTeam.name}}</a>
+                    </td>
+                    <td class="result">
+                        <a ng-if="match.isResult">{{match.homeGoals}}:{{match.awayGoals}}</a>
+                        <img class="future-match" ng-if="!match.isResult"
+                             src="${pageContext.request.contextPath}/resources/images/common/ball.png">
+                    </td>
+                    <td class="right">
+                        <a href="${pageContext.request.contextPath}/teams/teaminfo/{{match.awayTeam.dbid}}">{{match.awayTeam.name}}</a>
+                    </td>
+                </tr>
+
+            </table>
+        </div>
+
+
     </div>
 
 
--- webui/src/main/webapp/resources/js/teams/result.js
+++ webui/src/main/webapp/resources/js/teams/result.js
@@ -4,4 +4,118 @@
 
 soccerApp.controller("resultController",
     function resultController($scope, $http) {
+        $scope.competitions = [];
+        $scope.selectedCompetition = {};
+
+        $scope.map = [
+            {
+                date: "",
+                matches: []
+            }
+        ];
+
+        $scope.loadTournament = function (id) {
+            var requestParams = {
+                key: apiKey,
+                url: apiUrl + "league-tables?competition_id=" + id
+            };
+            $http.post("/getapidata", requestParams)
+                .then(function (data) {
+                    $scope.tournamentsData = JSON.parse(data.data);
+                    $.each($scope.tournamentsData, function (index, value) {
+                        value.index = index;
+                        $.each(value.leagueTable, function (teamIndex, teamValue) {
+                            teamValue.index = teamIndex;
+                        });
+                    });
+                })
+        };
+
+
+        $scope.getCompetitions = function () {
+
+            var teamIndex = 0;
+            $http.get("/teams/getcompetitions")
+                .then(function (data) {
+                    $scope.competitions = data.data;
+                    $.each($scope.competitions, function (index, value) {
+                        if (value.dbId == 36)
+                            teamIndex = index;
+                        value.region = " (" + value.region + ")";
+                    });
+                    $scope.selectedCompetition = $scope.competitions[teamIndex];
+                    $scope.loadMatches($scope.selectedCompetition.dbId);
+
+                    $("#selectedCompetition").css("display", "");
+                }, function (data) {
+                });
+        };
+
+        $scope.changeTeams = function (selectedCompetition) {
+            $scope.map = [];
+            $scope.loadMatches(selectedCompetition.dbId);
+        };
+
+        $scope.loadMatches = function (id) {
+            var requestParams = {
+                key: apiKey,
+                url: apiUrl + "matches?competition_id=" + id
+            };
+            $http.post("/getapidata", requestParams)
+                .then(function (data) {
+                    $scope.matches = JSON.parse(data.data);
+                    var longTime = new Date().getTime();
+                    $.each($scope.matches, function (index, value) {
+                        var matchDate = new Date(value.start);
+                        var date = matchDate.toLocaleString('ru', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric'
+                        });
+                        var time = matchDate.toLocaleString('ru', {
+                            hour: 'numeric',
+                            minute: 'numeric'
+                        });
+                        value.date = date;
+                        value.time = time;
+
+                        var find = false;
+                        var badMatch = false;
+
+                        $.each($scope.map, function (mapIndex, mapValue) {
+                            if (mapValue.date === value.date) {
+                                if (value.start < longTime && value.isResult) {
+                                    mapValue.matches.push(value);
+                                    find = true;
+                                } else if (value.start >= longTime) {
+                                    mapValue.matches.push(value);
+                                    find = true;
+                                } else if (value.start < longTime && !value.isResult) {
+                                    badMatch = true;
+                                }
+                            }
+
+                        });
+                        if (!find && !badMatch) {
+                            var obj = {
+                                date: value.date,
+                                matches: []
+                            };
+                            obj.matches.push(value);
+                            if (value.start < longTime && value.isResult)
+                                $scope.map.push(obj);
+                            else if (value.start < longTime && !value.isResult) {
+                                $scope.map.push(obj);
+                            } else if (value.start >= longTime) {
+                                $scope.map.push(obj);
+                            }
+                        }
+                    });
+                    $scope.map.splice(0, 1);
+                    $scope.map = $scope.map.reverse();
+                })
+        };
+
+
+        $scope.getCompetitions();
     });
